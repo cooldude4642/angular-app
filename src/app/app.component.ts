a86133f165f16b28e63af154a089c50454d9cd5f
cooldude4642 <cooldude4642@gmail.com>
2023-09-25 22:12:30 +0300

    feat: initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+/**
+ * Главный компонент приложения
+ */
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+})
+export class AppComponent {}
